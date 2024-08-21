d31cdb454f3cba639b996c6d6646dbeb098d5c31
AlfredPietraru <alfred.pietraru@gmail.com>
2024-08-21 11:21:55 +0300

    added joint bilateral filtering to the image

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ def pad_matrix(matrix):
                             (3, 3, 3, 3), mode='constant', value=0)
 
 
+def extract_model_result(y_features : torch.Tensor):
+    y_features = y_features.reshape(
+        shape=(tr.TOTAL_SIZE, tr.TOTAL_SIZE, 2))
+    u,v = y_features.chunk(2, dim=2)
+    u = pad_matrix(torch.squeeze(u))
+    v = pad_matrix(torch.squeeze(v))
+    return u, v
+    
+
+
 def yuv_to_rgb(y : torch.Tensor, u : torch.Tensor, v : torch.Tensor):
     y = y - 16
     u = u - 128
@@ -18,10 +28,7 @@ def yuv_to_rgb(y : torch.Tensor, u : torch.Tensor, v : torch.Tensor):
     B = 1.164 * y + 2.018 * u
     G = 1.164 * y - 0.813 * v - 0.391 * u
     R = 1.164 * y + 1.596 * v
-    R = R.byte()
-    B = B.byte()
-    G = G.byte()
-    return torch.stack([R, G, B], dim=-1)
+    return torch.stack([R.byte(), G.byte(), B.byte()], dim=-1)
 
 def evaluation(model):
     model.eval()
@@ -37,19 +44,16 @@ def evaluation(model):
                             ].reshape(tr.LOW_FEATURE_SIZE)
             features[index] = torch.cat((sub_area, descriptors[i][j]), 0)
             index += 1
-    result = torch.Tensor(model(features)).reshape(
-        shape=(tr.TOTAL_SIZE, tr.TOTAL_SIZE, 2))
-    u,v = result.chunk(2, dim=2)
-    u = pad_matrix(torch.squeeze(u))
-    np.savetxt('try.txt', np.round(u.detach().numpy(), 3))
-    v = pad_matrix(torch.squeeze(v))
+    u, v = extract_model_result(torch.Tensor(model(features)))
     return yuv_to_rgb(img, u, v)
 
 if __name__ == "__main__":
     model = DeepColorization()
     model.load_state_dict(torch.load("./model.pth"))    
     final_image = evaluation(model)
-    cv2.imshow("gata", final_image.numpy())
+    final_image = cv2.ximgproc.jointBilateralFilter(prep.return_gray_image(8),
+                                                     final_image.numpy(), 10, sigmaColor=15, sigmaSpace=15) 
+    cv2.imshow("gata", final_image)
     cv2.waitKey(0)
 
 
