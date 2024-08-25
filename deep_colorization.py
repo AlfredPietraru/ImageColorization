398ed430ee9b7f5daeb2d4a0e7f37765bc756790
AlfredPietraru <alfred.pietraru@gmail.com>
2024-08-25 10:38:52 +0300

    some changes added

--- deep_colorization.py
+++ deep_colorization.py
@@ -1,18 +1,33 @@
 from torch import nn
+import torch
+
+U_MAX = 0.436
+V_MAX = 0.615
 
 class DeepColorization(nn.Module):
-    def __init__(self, input_size = 81):
+    def __init__(self, input_size=81):
         super().__init__()
+
+        self.hidden1 = nn.Linear(input_size, input_size)
+        self.bn1 = nn.BatchNorm1d(input_size)
         
-        self.hidden1 = nn.Linear(input_size, input_size // 2)
-        self.hidden2 = nn.Linear(input_size // 2, input_size // 2)
+        self.hidden2 = nn.Linear(input_size, input_size // 2)
+        self.bn2 = nn.BatchNorm1d(input_size // 2)
+        
         self.hidden3 = nn.Linear(input_size // 2, input_size // 2)
+        self.bn3 = nn.BatchNorm1d(input_size // 2)
+        
         self.output = nn.Linear(input_size // 2, 2)
+        
         self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
+        self.leaky_relu = nn.LeakyReLU(0.1)
+        self.dropout = nn.Dropout(0.3)
+        self.tanh = nn.Tanh()
 
     def forward(self, x):
-        x = self.relu(self.hidden1(x))
-        x = self.relu(self.hidden2(x))
-        x = self.relu(self.hidden3(x))
-        return self.sigmoid(self.output(x))
+        x = self.leaky_relu(self.bn1(self.hidden1(x)))
+        x = self.leaky_relu(self.bn2(self.hidden2(x)))
+        x = self.leaky_relu(self.bn3(self.hidden3(x)))
+        x = self.tanh(self.output(x))
+        return x * torch.Tensor((0.436, 0.615))
+
--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 SIZE = (512, 512)
 PATH_IMAGES = "training_images/initial_images/"
@@ -24,11 +25,37 @@ def return_yuv_image(i : int):
     return cv2.split(img_yuv)
 
 
-for i in range(1, 9, 1):
-    image_name =  "image" + str(i) + ".jpg"
-    image_path = PATH_IMAGES + image_name
+
+
+# for i in range(88, 496, 1):
+#     image_name =  "image" + str(i) + ".png"
+#     image_path = PATH_IMAGES + image_name
+#     img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+#     resized_img = cv2.resize(img, SIZE)
+#     gray_image = cv2.cvtColor(resized_img, cv2.COLOR_RGB2GRAY)
+#     cv2.imwrite(new_color_img_path(i), resized_img)
+#     cv2.imwrite(new_gray_img_path(i), gray_image)
+
+def get_current_img_path(idx : int):
+    if (idx <= 99):
+        value = "00000" + str(idx) + ".png"
+    if (idx >= 100):
+        value = "0000" + str(idx) + ".png"
+    return PATH_COLOR_IMAGES + value
+
+def get_new_name(idx : int):
+    return PATH_COLOR_IMAGES + "color_image" + str(idx) + ".jpg"
+
+# files = os.listdir(PATH_COLOR_IMAGES)
+# for i in range(62, 495, 1):
+#     os.rename(get_new_name(i), get_new_name(i-1))
+
+# for i in range(88, 496, 1):
+#      os.rename(get_current_img_path(i), get_new_name(i))
+
+
+for i in range(1, 468, 1):
+    image_path = new_color_img_path(i)
     img = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    resized_img = cv2.resize(img, SIZE)
-    gray_image = cv2.cvtColor(resized_img, cv2.COLOR_RGB2GRAY)
-    cv2.imwrite(new_color_img_path(i), resized_img)
+    gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     cv2.imwrite(new_gray_img_path(i), gray_image)
